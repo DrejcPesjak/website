88f0e1c52093412bdbb0a0a60e2ea581b45f9714
DrejcPesjak <drejcpesjak.pesjak@gmail.com>
2019-06-16 11:02:33 +0200

    Initial commit

---
+++ js/lib.js
@@ -0,0 +1,26 @@
+console.log("V rit me piši!");
+/*
+$(document).ready(function () {
+ 
+	var dic = document.querySelectorAll('#dictionary li');
+	for(var a in dic) {
+		//console.log(a);
+		dic[a].onclick = markBooks();
+	}	
+
+});
+*/
+function markBooks() {
+	console.log(event.originalTarget.className);
+	var classA = event.originalTarget.className;
+	var books = document.querySelectorAll("li");
+	
+	for(var b in books) {
+		console.log(b, books[b]);
+		if(books[b].className == classA)
+			books[b].style.color = "blue";
+		else
+			books[b].style.color = "black";
+	}
+
+}
