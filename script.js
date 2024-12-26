e8027cc703684e3d7dfa1ed95d8a226c3fcb2981
DrejcPesjak <drejcpesjak.pesjak@gmail.com>
2024-12-26 02:10:53 +0100

    Events added

---
+++ script.js
@@ -0,0 +1,5 @@
+const navToggle = document.getElementById('nav-toggle');
+const mobileNav = document.getElementById('mobile-nav');
+navToggle.addEventListener('click', () => {
+    mobileNav.classList.toggle('hidden');
+});
