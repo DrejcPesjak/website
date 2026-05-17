d5d193b1446daac3ced64635de20f5cb8280df67
DrejcPesjak <drejcpesjak.pesjak@gmail.com>
2026-05-17 15:07:26 +0200

    GA and deck-vault fix

---
+++ analytics.js
@@ -0,0 +1,7 @@
+window.dataLayer = window.dataLayer || [];
+function gtag(){window.dataLayer.push(arguments);}
+
+gtag('js', new Date());
+gtag('config', 'G-C40SJWN8QX', {
+  cookie_domain: 'drejcpesjak.github.io'
+});
